efda572fa4073cd03321afaacb4866d59028abee
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-06-01 19:37:13 +0300

    fix(tests): apply opposite triggers

--- __tests__/papmap.test.ts
+++ __tests__/papmap.test.ts
@@ -11,21 +11,21 @@ describe('Monitored Collection', () => {
     sweep: mockMonitor
   });
 
-  it('should trigger monitor', () => {
+  it('should trigger monitor by default', () => {
     mockMonitor.mockClear();
     papmap.set(k, v, true);
-    papmap.get(k, true);
-    papmap.delete(k, true);
+    papmap.get(k);
+    papmap.delete(k,);
     papmap.sweep(() => true, true);
     expect(mockMonitor).toBeCalledTimes(4)
   });
 
   it('should not trigger monitor', () => {
     mockMonitor.mockClear();
-    papmap.set(k, v);
+    papmap.set(k, v, false);
     papmap.get(k, false);
-    papmap.delete(k);
-    papmap.sweep(() => true);
+    papmap.delete(k, false);
+    papmap.sweep(() => true, false);
     expect(mockMonitor).not.toBeCalled()
   });
 });
